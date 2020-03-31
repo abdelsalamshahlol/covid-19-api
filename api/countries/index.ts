b97c043a3fe8288467d8f54272d9282440de3239
Abdelsalam Shahlol <abdulsalamshahlol@gmail.com>
2020-03-31 22:20:12 +0200

    added arabic names to countries util and on API return [update]

--- api/countries/index.ts
+++ api/countries/index.ts
@@ -1,33 +1,35 @@
-import { NowResponse } from "@now/node";
+import {NowResponse} from "@now/node";
 
-import { countries, iso3, aliases } from "../../util/countries";
+import {countries, iso3, aliases, countriesAr} from "../../util/countries";
 import {
-  fetchFeatures,
-  attributeSpreader,
-  normalizeKeys,
-  matchCountryCode,
-  getIso3Code
+    fetchFeatures,
+    attributeSpreader,
+    normalizeKeys,
+    matchCountryCode,
+    getIso3Code
 } from "../../util/data";
-import { getEndpoint } from "../../util/endpoints";
-import { queryConfirmed } from "../../util/query";
+import {getEndpoint} from "../../util/endpoints";
+import {queryConfirmed} from "../../util/query";
 
 export default async (_, res: NowResponse) => {
-  const availableCountries = (
-    await fetchFeatures(getEndpoint("countryRegion"), queryConfirmed())
-  )
-    .map(attributeSpreader)
-    .map(normalizeKeys)
-    .map(d => d.countryRegion)
-    .map(countryRegion => {
-      const iso2Country =
-        countries[countryRegion] || aliases[countryRegion.toLowerCase()];
-      const iso3Country = iso3[iso2Country];
-      return {
-        name: countryRegion,
-        iso2: iso2Country,
-        iso3: iso3Country
-      };
-    });
-  availableCountries.sort((a, b) => (a.name < b.name ? -1 : 1));
-  res.json({ countries: availableCountries });
+    const availableCountries = (
+        await fetchFeatures(getEndpoint("countryRegion"), queryConfirmed())
+    )
+        .map(attributeSpreader)
+        .map(normalizeKeys)
+        .map(d => d.countryRegion)
+        .map(countryRegion => {
+            const iso2Country =
+                countries[countryRegion] || aliases[countryRegion.toLowerCase()];
+            const iso3Country = iso3[iso2Country];
+            const nameArabic = countriesAr[iso2Country];
+            return {
+                name: countryRegion,
+                iso2: iso2Country,
+                iso3: iso3Country,
+                name_ar: nameArabic
+            };
+        });
+    availableCountries.sort((a, b) => (a.name < b.name ? -1 : 1));
+    res.json({countries: availableCountries});
 };
