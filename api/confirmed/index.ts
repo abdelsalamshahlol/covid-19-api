6de7a2e47c01d5f21ed805e0ebccf7b8ffabc62f
Abdelsalam Shahlol <abdulsalamshahlol@gmail.com>
2020-04-01 17:07:34 +0200

    added name_ar property to confirmed endpoint

--- api/confirmed/index.ts
+++ api/confirmed/index.ts
@@ -1,27 +1,30 @@
-import { NowResponse, NowRequest } from "@now/node";
+import {NowResponse, NowRequest} from "@now/node";
 import {
-  fetchFeatures,
-  attributeSpreader,
-  normalizeKeys,
-  matchCountryCode,
-  getIso3Code
+    fetchFeatures,
+    attributeSpreader,
+    normalizeKeys,
+    matchCountryCode,
+    getIso3Code,
+    addArabicName
 } from "../../util/data";
-import { queryConfirmed } from "../../util/query";
-import { getEndpoint } from "../../util/endpoints";
+import {queryConfirmed} from "../../util/query";
+import {getEndpoint} from "../../util/endpoints";
 
 // const groupByCountryRegion
 
 export default async (request: NowRequest, response: NowResponse) => {
-  const shouldGroupByCountryRegion = request.query["byCountry"] === "true";
-  const endpoint = getEndpoint(
-    shouldGroupByCountryRegion
-      ? "countryRegion"
-      : (request.query.level as string)
-  );
-  const data = (await fetchFeatures(endpoint, queryConfirmed()))
-    .map(attributeSpreader)
-    .map(normalizeKeys)
-    .map(matchCountryCode)
-    .map(getIso3Code);
-  response.json(data);
+    const shouldGroupByCountryRegion = request.query["byCountry"] === "true";
+    const endpoint = getEndpoint(
+        shouldGroupByCountryRegion
+            ? "countryRegion"
+            : (request.query.level as string)
+    );
+    const data = (await fetchFeatures(endpoint, queryConfirmed()))
+        .map(attributeSpreader)
+        .map(normalizeKeys)
+        .map(matchCountryCode)
+        .map(getIso3Code)
+        .map(addArabicName);
+    // console.log({data})
+    response.json(data);
 };
